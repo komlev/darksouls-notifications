8558e9b57312ef5de39858b33ab4428c426bdf4a
Valeriy Komlev <komlev88@gmail.com>
2025-09-14 01:24:48 +0200

    adding pack script and README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # darksouls-notifications
+
 Dark Souls themed email notification extension
+
+## How to run
+
+1. Clone the repository: `git clone https://github.com/username/darksouls-notifications.git`
+2. Install node.js on your system if it's not already installed.
+3. Navigate to the project directory
+4. Run `npm run pack:chrome` for chrome extension or `npm run pack:firefox` for firefox extension.
+5. Extensions will be generated in the `dist` folder.
+
+Chrome needs to pack extension separately through "Pack Extension" command on "chrome://extensions/" page.
+Just unpack chrome.zip from dist folder and pack it with chrome.
---
+++ scripts/pack.js
@@ -0,0 +1,95 @@
+import fs from "fs";
+import path from "path";
+import childProcess from "child_process";
+
+const VALID_TYPES = ["firefox", "chrome"];
+const NECESSARY_FILES = [
+  "OptimusPrinceps.ttf",
+  "script.js",
+  "sound.mp3",
+  "style.css",
+];
+const PACKAGE_NAMES = {
+  firefox: "firefox.xpi",
+  chrome: "chrome.zip",
+};
+
+const [, , incomingType] = process.argv;
+// JUST CHECKING FOR PROPER TYPE AND ALL SINCE WE ARE WORKING WITH FILES HERE
+if (!VALID_TYPES.includes(incomingType)) {
+  console.log(
+    incomingType ? `Unknown type "${incomingType}"` : "Type is not provided",
+  );
+  process.exit(1);
+}
+
+const createTmpDirectory = (t) => {
+  // CREATE CLEAN TMP DIRECTORY
+  const tmpDir = `./tmp_${new Date().getTime()}_${t}`;
+  if (fs.existsSync(tmpDir)) {
+    // SHOULD NOT HAPPEN
+    console.log(`Directory ${tmpDir} already exists`);
+    process.exit(1);
+  }
+
+  fs.mkdirSync(tmpDir);
+  return tmpDir;
+};
+
+const cleanup = (dir) => {
+  // REMOVE TMP DIRECTORY
+  try {
+    fs.rmSync(dir, { recursive: true, force: true });
+  } catch (error) {
+    console.error(`Error cleaning up directory ${dir}: ${error}`);
+  }
+};
+
+const zipDirectory = (dir, zipFileName) => {
+  childProcess.execSync(`zip -r ${zipFileName} *`, { cwd: dir });
+};
+
+const finalize = (zip, tmpDir) => {
+  // FINALIZE
+};
+
+const packExtension = (type) => {
+  // GET CLEAN TMP DIRECTORY
+  const tmpDirectory = createTmpDirectory(type);
+
+  // COPY SHARED FILES TO TMP DIRECTORY
+  NECESSARY_FILES.forEach((file) => {
+    const sourcePath = path.resolve(file);
+    const destinationPath = path.resolve(tmpDirectory, file);
+    fs.copyFileSync(sourcePath, destinationPath);
+  });
+
+  // NOW COPY TYPE FILES TO TMP DIRECTORY
+  fs.readdirSync(path.resolve(`./${type}`))
+    .map((file) => file)
+    .filter((file) => file.endsWith(".json") || file.endsWith(".png"))
+    .forEach((file) => {
+      const sourcePath = path.resolve(`${type}/${file}`);
+      const destinationPath = path.resolve(tmpDirectory, file);
+      fs.copyFileSync(sourcePath, destinationPath);
+    });
+
+  // PACK STUFF UP INTO ZIP FILE
+  const zipFile = `${type}.zip`;
+  zipDirectory(tmpDirectory, zipFile);
+  // MAKE DIST DIRECTORY IF NOT EXISTS
+  if (!fs.existsSync("./dist")) {
+    fs.mkdirSync("./dist");
+  }
+
+  // MOVE ZIP FILE TO DIST DIRECTORY
+  const packageName = fs.renameSync(
+    `./${tmpDirectory}/${zipFile}`,
+    path.resolve("./dist", PACKAGE_NAMES[type]),
+  );
+
+  // // CLEANUP
+  cleanup(tmpDirectory);
+};
+
+packExtension(incomingType);
